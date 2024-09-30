5e0ba191be36208865eb36cc05bc8e2357b3af58
abdellahbenrhouma <benrhoumaabdellah@gmail.com>
2024-09-30 22:28:06 +0100

    added vercel.json

--- taureauvache_server/server.js
+++ taureauvache_server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://taureauvache-client.vercel.app/",
     credentials: true
   }
 });
@@ -17,7 +17,7 @@ const io = new Server(server, {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://taureauvache-client.vercel.app/",
   credentials: true
 }));
 
