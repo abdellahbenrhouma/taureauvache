2532fbbeab626790ba7f7578586f7eff26755235
abdellahbenrhouma <benrhoumaabdellah@gmail.com>
2024-09-30 23:07:35 +0100

    last try

--- taureauvache/src/App.jsx
+++ taureauvache/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('https://taureauvache.onrender.com/'); // Connect to the backend
+const socket = io('http://localhost:3000'); // Connect to the backend
 
 export default function Component() {
   const [secretNumber, setSecretNumber] = useState('');
